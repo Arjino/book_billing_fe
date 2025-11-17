af358a444d93637e6f504e6f217c80e33ab2003a
sssinha0 <b180043@nitsikkim.ac.in>
2025-11-17 23:10:16 +0530

    bug fix

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,12 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    // Dynamic invoice route should be client-rendered because it contains parameters
+    // and we don't want to prerender every possible invoice id.
+    path: 'invoice/:id',
+    renderMode: RenderMode.Client
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
